d1d4c75949835c5d80fefdd6b4eb975d4439e7c8
loner98 <1987385855@qq.com>
2025-12-04 22:23:02 +0800

    Site updated: 2025-12-04 22:23:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/03/hello-world/","2025/12/04/The-beginning/"];function toRandomPost(){
+var posts=["2025/12/04/The-beginning/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
