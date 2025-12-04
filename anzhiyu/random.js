01ae2471e98254cd40ba5aecf59be119bbfc95e9
loner98 <1987385855@qq.com>
2025-12-04 09:37:51 +0800

    Site updated: 2025-12-04 09:37:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/03/hello-world/","2025/12/03/tags/"];function toRandomPost(){
+var posts=["2025/12/03/hello-world/","2025/12/03/tags/","2025/12/04/自我介绍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
