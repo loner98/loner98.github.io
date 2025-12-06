a2e200fc42ca756bd1a8417b67f6529240c4a203
loner98 <1987385855@qq.com>
2025-12-06 15:12:40 +0800

    Site updated: 2025-12-06 15:12:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/04/The-beginning/","2025/12/05/具身智能/"];function toRandomPost(){
+var posts=["2025/12/05/具身智能/","2025/12/04/The-beginning/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
