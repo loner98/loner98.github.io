c75a4e177d814550e705b5e668b38fdc26806e21
loner98 <1987385855@qq.com>
2025-12-15 21:40:04 +0800

    Site updated: 2025-12-15 21:40:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/04/The-beginning/","2025/12/05/具身智能/","2025/12/09/一些docker命令/","2025/12/13/写点什么吧/"];function toRandomPost(){
+var posts=["2025/12/15/C-类与结构体/","2025/12/04/The-beginning/","2025/12/09/一些docker命令/","2025/12/05/具身智能/","2025/12/13/写点什么吧/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
