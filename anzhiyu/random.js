55ec784b777fad63070bbcecf008461f11fc757a
loner98 <1987385855@qq.com>
2025-12-30 16:32:32 +0800

    Site updated: 2025-12-30 16:32:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/04/The-beginning/","2025/12/15/C++笔记/","2025/12/09/一些docker命令/","2025/12/05/具身智能/","2025/12/19/一些要记住的事情/","2025/12/18/私密树洞/","2025/12/13/写点什么吧/"];function toRandomPost(){
+var posts=["2025/12/15/C++笔记/","2025/12/04/The-beginning/","2025/12/19/一些要记住的事情/","2025/12/09/一些docker命令/","2025/12/13/写点什么吧/","2025/12/05/具身智能/","2025/12/18/私密树洞/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
